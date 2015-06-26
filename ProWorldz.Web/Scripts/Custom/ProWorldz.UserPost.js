49f586c985c7cd62a5a78d7c35d7540dd4c3f263
unknown <vasunagpal@vasu368301.Nagarro.local>
2015-06-26 17:14:06 +0530

    UiI changes

--- ProWorldz.Web/Scripts/Custom/ProWorldz.UserPost.js
+++ ProWorldz.Web/Scripts/Custom/ProWorldz.UserPost.js
@@ -2,12 +2,18 @@
 proWorld.userPost = proWorld.userPost || {};
 
 
+proWorld.userPost.BindUIevents = function () {
+
+    $('#divPost').hide();
+    CKEDITOR.replace('editor1');
+    $("#createPost").click(function () {
+        $('#divPost').toggle("slow");
+    })
+
+}
+
 proWorld.userPost.hideMessage = function (data) {
     $(data).hide("slow")
 }
 
-proWorld.userPost.initCkEditor = function () {
-    CKEDITOR.replace('editor1');
-}
-
-proWorld.userPost.initCkEditor();
+proWorld.userPost.BindUIevents();
