f96887cf5879a1eba13991c9676b8a10b48a960f
Vasu Nagpal <vasunagpal@vasu368301.Nagarro.local>
2015-09-04 02:44:14 +0530

    chatin module front end in place

--- ProWorldz.Web/Startup.cs
+++ ProWorldz.Web/Startup.cs
@@ -1,9 +1,7 @@
 ﻿using Owin;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using Microsoft.Owin;
 
+[assembly: OwinStartup(typeof(ProWorldz.Web.Startup))]
 namespace ProWorldz.Web
 {
     public class Startup
