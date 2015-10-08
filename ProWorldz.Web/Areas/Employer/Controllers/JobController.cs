4bd875fba4feb62aec7c832c184a784579c6d661
Himanshu Rastogi <hr260389@gmail.com>
2015-10-09 01:19:31 +0530

    commit

---
+++ ProWorldz.Web/Areas/Employer/Controllers/JobController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProWorldz.Web.Areas.Employer.Controllers
+{
+    public class JobController : Controller
+    {
+        //
+        // GET: /Employer/Job/
+
+        public ActionResult JobPost()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ ProWorldz.Web/Areas/Employer/Scripts/Custom/employer.job.js
@@ -0,0 +1,3 @@
+﻿(function (jobVM) {
+
+})(JobPostVM);
